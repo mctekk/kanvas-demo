25703f5bd0460150c0ecc8df5d964df3a18d2d91
RivierGrullon <grullonrivier@gmail.com>
2024-03-06 16:28:48 -0400

    feat: Add redirect configuration and update home page content

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: "/sign-in",
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-'use client'
-import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  useRouter().push('/sign-in');
-  return(<></>)
+  return(<>Hello World</>)
 }
