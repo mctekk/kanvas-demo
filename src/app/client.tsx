6cc5b73cf2e5f9047a316b2f0fcce1913f4c5769
RivierGrullon <grullonrivier@gmail.com>
2024-03-06 16:23:08 -0400

    fix: Update dependency version for @kanvas/phoenix

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import { client } from '@/kanvas-client';
-import { BaseTheme } from '@kanvas/phoenix';
 import { ClientCoreStore } from '@kanvas/phoenix/dist/client';
 import { PropsWithChildren } from 'react';
 
 export default function RootClientLayout({ children }: PropsWithChildren) {
   return (
-    <ClientCoreStore sdk={client} theme={BaseTheme}>
+    <ClientCoreStore sdk={client} >
       <>{children}</>
     </ClientCoreStore>
   );
