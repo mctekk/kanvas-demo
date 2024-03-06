efbce53e03d9a2eee4f5698ac0e84f00ed519190
RivierGrullon <grullonrivier@gmail.com>
2024-03-06 16:30:03 -0400

    refactor: Update async redirects in next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async redirects() {
-      return [
-        {
-          source: "/",
-          destination: "/sign-in",
-          permanent: true,
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/sign-in',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
